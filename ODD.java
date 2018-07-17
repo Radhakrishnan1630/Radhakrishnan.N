27650a8f13a24e050281aacb784d752758ff4b18
Radhakrishnan1630 <41100309+Radhakrishnan1630@users.noreply.github.com>
2018-07-17 09:18:30 +0530

    Add files via upload

---
+++ ODD.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ODD {
+	public static void main(String[]args){
+		int a;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		if(a%2==0){
+			System.out.println("even");
+		}
+		else
+			System.out.println("odd");
+	}
+}
