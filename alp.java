a29cd065b77269f878a465f320a173466416f254
Radhakrishnan1630 <41100309+Radhakrishnan1630@users.noreply.github.com>
2018-07-17 09:42:23 +0530

    Add files via upload

---
+++ alp.java
@@ -0,0 +1,15 @@
+package alp;
+import java.util.*;
+public class alp {
+	public static void main(String[]args){
+		char c;
+		Scanner s=new Scanner(System.in);
+		c=s.next().charAt(0);
+		if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+			System.out.println("the given character is an Alphabets");
+		}else
+		{
+		System.out.println("Not an alphabets");
+		}
+	}
+}
